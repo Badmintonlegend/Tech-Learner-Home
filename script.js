2c508b68580e3a6fa7a1acde8e82274044bde3ba
Badmintonlegend <sraval2011@gmail.com>
2025-03-07 21:19:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 var x = prompt("Register your name for the quiz");
     document.getElementById("demo").innerHTML = "Hello " + x + ", Welcome to Tech Learner. Your name is now registered";
+function fg(){
+    window.open("https://badmintonlegend.github.io/Tech-Learner-Home/","_self"); 
+}
+function gd(){
+    window.open("https://badmintonlegend.github.io/Tech-Learner-Home/","_self");
+}
+function hh(){
+    window.open("https://badmintonlegend.github.io/Tech-Learner-Home/","_self");
+}
+function nj(){
+    window.open("https://badmintonlegend.github.io/Tech-Learner-Home/","_self");
+}
+function mk(){
+    window.open("https://badmintonlegend.github.io/Tech-Learner-Home/","_self");
+}
